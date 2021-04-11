1280390cb6563c8aa6a9193ff7ff6477abde66e5
vish <00000vish@users.noreply.github.com>
2021-04-11 13:39:07 -0500

    TODO: LOGGING

---
+++ StartupManager/Logger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartupManager
+{
+    static class Logger
+    {
+        static string path = "";
+        static void writeLog(string path)
+        {
+            System.IO.File.AppendAllLines("test.txt", new string[] {path});
+        }
+    }
+}
